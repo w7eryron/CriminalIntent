a42acb175a587f6fa07b25bc61d21bce477dd908
toddaton <toddaton@hotmail.com>
2014-02-09 09:08:06 -0800

    Initial commit of CriminalIntent project.

---
+++ CriminalIntent/src/com/toddaton/criminalintent/CrimeLab.java
@@ -0,0 +1,43 @@
+package com.toddaton.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.content.Context;
+
+public class CrimeLab {
+	private static CrimeLab sCrimeLab;
+	private Context mAppContext;
+	private ArrayList<Crime> mCrimes;
+	
+	private CrimeLab (Context appContext) {
+		mAppContext = appContext;
+		mCrimes = new ArrayList<Crime>();
+		
+		// TODO: remove this after I can add crimes
+		for (int i = 0; i < 100; i++) {
+			Crime c = new Crime();
+			c.setTitle("Crime # " + i);
+		}
+	};
+	
+	public static CrimeLab get(Context c) {
+		if (sCrimeLab == null) {
+			sCrimeLab = new CrimeLab(c.getApplicationContext());
+		}
+		return sCrimeLab;
+	}
+	
+	public ArrayList<Crime> getCrimes() {
+		return mCrimes;
+	}
+	
+	public Crime getCrime(UUID id) {
+		for (Crime c : mCrimes) {
+			if (c.getId().equals(id)) {
+				return c;
+			}
+		}
+		return null;
+	}
+}
---
+++ CriminalIntent/src/com/toddaton/criminalintent/CrimeListFragment.java
@@ -0,0 +1,18 @@
+package com.toddaton.criminalintent;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class CrimeListFragment extends ListFragment {
+	private ArrayList<Crime> mCrimes;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActivity().setTitle(R.string.crimes_title);
+		
+		mCrimes = CrimeLab.get(getActivity()).getCrimes();
+	}
+}
