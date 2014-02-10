8fc44ce1af21107bb9a03ca6bcb3ef0a03071477
toddaton <toddaton@hotmail.com>
2014-02-10 08:50:29 -0800

    CrimeActivity extends SingleFragmentActivity instead of FragmentActivity.

--- CriminalIntent/src/com/toddaton/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/toddaton/criminalintent/CrimeActivity.java
@@ -1,23 +1,11 @@
 package com.toddaton.criminalintent;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fragment);
-		
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
-		
-		if (fragment == null) {
-			fragment = new CrimeFragment();
-			fragmentManager.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-		}
+	protected Fragment createFragment() {
+		return new CrimeFragment();
 	}
 }
